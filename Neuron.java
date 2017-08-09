9750df997abb8843b84d44dcd0977daef2f75cc4
Ellen <chenellen007@gmail.com>
2017-08-09 00:28:21 -0400

    Made Changes to Neuron

--- Neuron.java
+++ Neuron.java
@@ -2,4 +2,22 @@
  * Created by Reece Martin on 2017-08-08.
  */
 public class Neuron {
+    Weight[] parents;
+    Weight[] children;
+    char type = 'n';
+    
+    public Neuron (){
+        parents = null;
+        children = null;
+    }
+    
+    public Neuron (int num){
+        parents = new Weight[num];
+        children = new Weight[num];
+    }
+    
+    public Neuron (Weight[] p, Weight[] c){
+        parents = p;
+        children = c;
+    }
 }
